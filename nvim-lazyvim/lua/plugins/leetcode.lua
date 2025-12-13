200f67f86ba6c880370764f0b10e0a9dfbb9c441
taocw <taocw@foxmail.com>
2025-12-13 19:46:40 +0800

    [neovim]: lazyvim version

---
+++ nvim-lazyvim/lua/plugins/leetcode.lua
@@ -0,0 +1,16 @@
+return {
+  "kawre/leetcode.nvim",
+  build = ":TSUpdate html", -- if you have `nvim-treesitter` installed
+  dependencies = {
+    -- include a picker of your choice, see picker section for more details
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+  },
+  lazy = "leetcode.nvim" ~= vim.fn.argv(0, -1),
+  opts = {
+    lang = "c",
+    cn = {
+      enabled = true,
+    },
+  },
+}
